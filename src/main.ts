da3a0501153cbf26ed162ee141ea65d1e42aeab2
JhonFac <maserati92@hotmail.com>
2024-01-23 15:17:45 -0500

    Port

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 console.log('Inicia el proceso')
-// console.log($PORT)
+console.log($PORT)
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  // await app.listen(3000);
+  await app.listen($PORT);
 }
 bootstrap();
